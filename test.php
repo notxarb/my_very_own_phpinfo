e96363e4c52cc4c23dd368d20f39e16d170ca035
Braxton Huggins <braxton@delorum.com>
2011-07-01 12:40:52 -0600

    test for gd

--- test.php
+++ test.php
@@ -2,18 +2,18 @@
 header("Content-Type: image/png");
 
 
-$x = 1000;
-$y = 1000;
+$x = 100000;
+$y = 100000;
 
 $gd = imagecreatetruecolor($x, $y);
  
-$corners[0] = array("x" => 500, "y" =>  10);
-$corners[1] = array("x" =>   0, "y" => 990);
-$corners[2] = array("x" => 1000, "y" => 990);
+$corners[0] = array("x" => 50000, "y" =>  10);
+$corners[1] = array("x" =>   0, "y" => 99990);
+$corners[2] = array("x" => 100000, "y" => 99990);
 
 $red = imagecolorallocate($gd, 255, 0, 0);
 
-for ($i = 0; $i < 100000; $i++) {
+for ($i = 0; $i < 100000000; $i++) {
   imagesetpixel($gd, round($x),round($y), $red);
   $a = rand(0, 2);
   $x = ($x + $corners[$a]["x"]) / 2;
